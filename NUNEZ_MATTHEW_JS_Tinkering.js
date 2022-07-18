f7d5571db9fa826d596ba064dd6694194bc3767d
Matthew Nunez <matthewbnunez@.com>
2022-07-17 22:10:22 -0700

    initial commit

---
+++ NUNEZ_MATTHEW_JS_Tinkering.js
@@ -0,0 +1,10 @@
+var childHeight = 36;
+function displayIfChildIsAbleToRideTheRollerCoaster(height) {
+    if (height > 52) {
+        console.log("Get on that ride, kiddo!");
+    }
+    else {
+        console.log("Sorry kiddo. Maybe next year.");
+    }
+}
+displayIfChildIsAbleToRideTheRollerCoaster(childHeight);
